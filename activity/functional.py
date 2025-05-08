0a5fffe73af18c6207ba76ecc87c719faafeb220
Max <imax.mr@gmail.com>
2025-05-08 11:10:04 -0700

    Completed Activity

--- activity/functional.py
+++ activity/functional.py
@@ -1,9 +1,16 @@
+from functools import reduce
+
 # Wave 1
 # Write a function that takes in a list of words and returns the shortest
 # one. (assume no ties)
 
+# def shortest_word(words):
+#     min_length = min(words, key=lambda x: len(x))
+#     return min_length
+
 def shortest_word(words):
-    pass
+    min_length = reduce(lambda x, y: x if len(x) < len(y) else y, words)
+    return min_length
 
 # SUPER CHALLENGE: use functools.reduce
 # Only try this after completing the rest of the activity
@@ -13,13 +20,15 @@ def shortest_word(words):
 #     pass    
 
 
+
 # Wave 2
 # Write a function that takes in a list of numbers and returns a new list
 # containing only the ones that are even, in the same order.
 # Hint: remember to convert back to a list!
 
 def even_nums(nums):
-    pass
+    even_only = filter(lambda x: x % 2 == 0, nums)
+    return list(even_only)
 
 # Wave 3
 # Write a function that takes in a list of numbers and returns a new list
@@ -27,7 +36,8 @@ def even_nums(nums):
 # Hint: remember to convert back to a list!
 
 def squares(nums):
-    pass
+    squared = map(lambda x: x ** 2, nums)
+    return list(squared)
 
 # Wave 4
 # Write a function that accepts a word, a function, and the name of that 
@@ -35,7 +45,7 @@ def squares(nums):
 # "The result of applying FUNCTION_NAME to WORD is RESULT"
 
 def report(word, function, function_name):
-    pass
+    return f"The result of applying {function_name} to {word} is {function(word)}"
 
 # Wave 5
 # Write a function that takes a list of passwords and returns a list of only 
@@ -43,5 +53,6 @@ def report(word, function, function_name):
 # The returned list should be sorted by in order of increasing length.
 
 def sorted_valid_passwords(passwords):
-    pass
+    result = filter(lambda x: x.isalpha() == False, sorted(passwords, key=lambda x: len(x)))
+    return list(result)
 
